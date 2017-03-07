9953098e16ada11d6732091193ac35435695dda5
Kent Watsen <kent@watsen.net>
2017-03-07 15:37:43 -0500

    checking in what I have

--- refs/gen-trees.sh
+++ refs/gen-trees.sh
@@ -1,7 +1,3 @@
-pyang -p ../ -f tree --tree-print-groupings ../ietf-ssh-client\@*.yang > ietf-ssh-client-tree.txt.tmp
-pyang -p ../ -f tree --tree-print-groupings ../ietf-ssh-server\@*.yang > ietf-ssh-server-tree.txt.tmp
+pyang -p ../ -f tree --tree-line-length 71 --tree-print-groupings ../ietf-ssh-client\@*.yang > ietf-ssh-client-tree.txt
+pyang -p ../ -f tree --tree-line-length 71 --tree-print-groupings ../ietf-ssh-server\@*.yang > ietf-ssh-server-tree.txt
 
-fold -w 71 ietf-ssh-client-tree.txt.tmp > ietf-ssh-client-tree.txt
-fold -w 71 ietf-ssh-server-tree.txt.tmp > ietf-ssh-server-tree.txt
-
-rm *-tree.txt.tmp
