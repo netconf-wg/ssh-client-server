f40116dfe5dd34b7a18a5806bff681bff3df49da
kwatsen <kent@watsen.net>
2020-02-19 15:04:34 -0500

    reordered import statements, added FIXMEs prepping for new 'must' statements, added YANGLINT_TEST for GitHub issue

--- refs/validate-all.sh
+++ refs/validate-all.sh
@@ -34,7 +34,9 @@ rm $name
 echo "Testing ex-ssh-client-keystore.xml..."
 name=`ls -1 ../ietf-ssh-client\@*.yang | sed 's/\.\.\///'`
 sed 's/^}/container ssh-client { uses ssh-client-grouping; }}/' ../ietf-ssh-client\@*.yang > $name
-yanglint -m -s ../ietf-crypto-types\@*.yang ../ietf-truststore\@*.yang ../ietf-keystore\@*.yang ../ietf-ssh-common\@*.yang ./ietf-origin.yang $name ex-ssh-client-keystore.xml ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml
+command="yanglint -m -s ../ietf-crypto-types\@*.yang ../ietf-truststore\@*.yang ../ietf-keystore\@*.yang ../ietf-ssh-common\@*.yang ./ietf-origin.yang $name ex-ssh-client-keystore.xml ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml"
+#echo $command
+`$command`
 rm $name
 
 echo "Testing ex-ssh-server-local.xml..."
@@ -46,6 +48,8 @@ rm $name
 echo "Testing ex-ssh-server-keystore.xml..."
 name=`ls -1 ../ietf-ssh-server\@*.yang | sed 's/\.\.\///'`
 sed 's/^}/container ssh-server { uses ssh-server-grouping; }}/' ../ietf-ssh-server\@*.yang > $name
-yanglint -m -s ../ietf-crypto-types\@*.yang ../ietf-truststore\@*.yang ../ietf-keystore\@*.yang ../ietf-ssh-common\@*.yang ./ietf-origin.yang $name ex-ssh-server-keystore.xml ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml
+command="yanglint -m -s ../ietf-crypto-types\@*.yang ../ietf-truststore\@*.yang ../ietf-keystore\@*.yang ../ietf-ssh-common\@*.yang ./ietf-origin.yang $name ../../trust-anchors/refs/ex-truststore.xml ../../keystore/refs/ex-keystore.xml ex-ssh-server-keystore.xml"
+#echo $command
+`$command`
 rm $name
 
